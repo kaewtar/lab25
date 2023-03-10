4edca728cec1b1c07fde3f85b2a5d61954fc1aa6
kaewtar <kaewtarcmu@gmail.com>
2023-03-10 16:54:28 +0700

    done

--- lab25_1.cpp
+++ lab25_1.cpp
@@ -63,4 +63,49 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int t){
+	
+	Node *current = root;
+	if(t == 0){
+		root = root->next;
+		delete current;
+	}
+	else{
+		for(int i = 0; i < t-1;i++){
+		current = current->next;
+	}
+	Node *temp = current->next;
+	current->next = current->next->next;
+	delete temp;
+	}
+	size--;
+	
+}
+int main()
+{
+	List myList = {0,0};
+myList.append(5);
+myList.append(7);
+myList.append(11);
+myList.append(4);
+myList.append(12);
+myList.append(45);
+
+myList.insert(29,2);
+myList.insert(33,3);
+myList.insert(77,5);
+myList.insert(69,0);
+myList.show();
+cout << "\n"; 
+
+myList.remove(2);
+myList.show();
+cout << "\n"; 
+myList.remove(4);
+myList.show();
+cout << "\n";
+myList.remove(0);
+myList.show();
+cout << "\n";
+return 0;
+}
